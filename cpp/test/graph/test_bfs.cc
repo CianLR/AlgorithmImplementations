f0f0e970f8343ad9c325031234a18a34c46bfafa
CianLR <cian.ruane1@gmail.com>
2018-05-24 17:31:06 +0100

    Use custom Google Test image for CI and add new BFS unit test

--- cpp/test/graph/test_bfs.cc
+++ cpp/test/graph/test_bfs.cc
@@ -13,3 +13,23 @@ TEST(BFSTest, TestSingleNode) {
   ASSERT_EQ(BFS(graph, 0, 10), 0);
 }
 
+TEST(BFSTest, TestMultiNodes) {
+  vector<Node> graph = {{10}, {20}, {30}, {40}};
+  graph[0].AddAdj(1);
+  graph[0].AddAdj(2);
+  graph[1].AddAdj(3);
+  graph[2].AddAdj(3);
+
+  // Start at 0 and traverse normally
+  ASSERT_EQ(BFS(graph, 0, 40), 3);
+  ASSERT_EQ(BFS(graph, 0, 30), 2);
+
+  // Start at 1, node 3 is reachable but 2 isn't.
+  ASSERT_EQ(BFS(graph, 1, 40), 3);
+  ASSERT_EQ(BFS(graph, 1, 30), -1);
+
+  // Start at 3, nothing reachable except self.
+  ASSERT_EQ(BFS(graph, 3, 40), 3);
+  ASSERT_EQ(BFS(graph, 3, 30), -1);
+}
+
